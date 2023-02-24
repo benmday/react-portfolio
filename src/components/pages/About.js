6462060739b6625e100824d33b5576a870e307ea
benmday <benjaminmichaelday@gmail.com>
2023-02-23 19:32:54 -0800

    updated gitignore

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,39 +1,12 @@
 import React from "react";
-import profilePicture from "../../portfolio-pic.jpeg";
-
-const BenDay = `{
-  livesIn: "Port Orchard, WA",
-  age: 27,
-  favoriteColor: "purple",
-  hobbies: ["enjoying foreign foods", "soccer", "record collecting", "rock climbing"],
-  pets: [
-    {
-    type: "cat",
-    name: "Ferguson"
-    },
-    {
-    type: "cat",
-    name: "Archie"
-    },
-    {
-    type: "cat",
-    name: "Posie"
-    },
-    {
-    type: "cat",
-    name: "Ollie"
-    },
-  ]
-}`
+import profilePicture from "../../profile-pic.jpeg";
+import portfolioCode from "../../portfolio-code.png";
 
 export default function About() {
   return (
     <div className="about">
       <img id="profile-pic" src={profilePicture} alt="Ben Day headshot"></img>
-      <p>
-        const BenDay = {BenDay}
-      </p>      
+      <img id="portfolio-code" src={portfolioCode} alt="About Ben Day code snippet"></img>
     </div>
   );
 }
-
