b84f582b267ba71f2000cf9d4b89e92aa8394a16
benmday <benjaminmichaelday@gmail.com>
2023-03-12 15:27:01 -0700

    added resume img, download, and styling

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
+import resumeJpg from "../../images/ben-day-resume.jpg";
+import resumePdf from "../../images/ben-day-resume.pdf";
 
 export default function Resume() {
   return (
     <div className="resume">
       <h4>Resume</h4>
-      <p>
-       Coming soon...
-      </p>
+      <img alt="Ben Day resume" src={resumeJpg} className="img-fluid"></img>
+      <a id="resume-pdf" href={resumePdf}>
+        <button type="button" className="btn btn-dark ms-3" id="download">
+          Download
+        </button>
+      </a>
     </div>
   );
 }
